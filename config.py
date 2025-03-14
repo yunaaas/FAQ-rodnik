23e60a51937025c1ba1010ebf65277dbdf038916
https://github.com/yunaaas/futoji-mobile.git <linkov.totosa2@bk.ru>
2025-03-14 23:27:08 +0300

    fix bot

--- config.py
+++ config.py
@@ -3,10 +3,15 @@
 '''
 import os
 from dotenv import load_dotenv
+from aiogram import Bot
+
 
 load_dotenv()
 
 API_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=API_TOKEN)
+
 
 if not API_TOKEN:
     raise ValueError("BOT_TOKEN не найден в переменных окружения!")
